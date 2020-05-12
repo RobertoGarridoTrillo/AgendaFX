c212d145f15e68ad3179e1f65e8608119a1d3202
RobertoGarridoTrillo <robegt72@hotmail.com>
2020-05-12 13:23:27 +0200

    I finished it(halfway) and put it up.

---
+++ src/addresses/model/PersonListWrapper.java
@@ -0,0 +1,36 @@
+package addresses.model;
+
+import addresses.model.Person;
+import java.util.List;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+
+/**
+ * Helper class to wrap a list of persons. This is used for saving the
+ * list of persons to XML.
+ *
+ * @author Roberto Garrido Trillo
+ */
+@XmlRootElement(name = "persons")
+public class PersonListWrapper
+{
+
+  private List<Person> persons;
+
+  @XmlElement(name = "person")
+  public List<Person> getPersons()
+  {
+    return persons;
+  }
+
+
+  public void setPersons(List<Person> persons)
+  {
+    this.persons = persons;
+  }
+
+
+}
+
